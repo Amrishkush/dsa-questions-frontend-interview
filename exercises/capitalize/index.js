1cdb8adcc043da9c7b7bfebdf9ca5bb6156a8a13
Amrish Pratap Singh <amrishkush@gmail.com>
2024-04-07 14:20:40 +0530

    capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -6,7 +6,30 @@
 //   capitalize('a short sentence') --> 'A Short Sentence'
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
+                            
+                        //solution 1        Done completely by me
+// function capitalize(str) {
+//     let capitalizedArray =[]
+//     for(let element of str.split(" ")){
+//         capitalizedArray.push(element[0].toUpperCase() + element.slice(1))
+//     }
 
-function capitalize(str) {}
+//     return capitalizedArray.join(" ")
+// }
 
+                      //solution 2
+function capitalize(str){
+    let result = str[0].toUpperCase()   //problem is first character we have to uppercase separately
+      //for of cant be used here because we iterate from 2 no and character on which left is space is capitalized
+    for(let i = 1; i < str.length; i++){   //will start from second leaving first 
+        if(str[i-1] === " "){
+            result += str[i].toUpperCase()
+        } else {
+            result += str[i]
+        }
+    }
+      return result
+}
+
+console.log(capitalize("amrish pratap singh"))
 module.exports = capitalize;
