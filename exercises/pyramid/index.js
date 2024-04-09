92f3491935d8f2c504c73cca0666a89f102796a7
Amrish Pratap Singh <amrishkush@gmail.com>
2024-04-09 14:19:34 +0530

    steps and pyramid with recursion also

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,41 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// function pyramid(n) {
+//   let midpoint = Math.floor((2 * n - 1) / 2);
+//   for (let row = 0; row < n; row++) {
+//     let stair = "";
+//     for (let column = 0; column < 2 * n - 1; column++) {
+//       if (midpoint - row <= column && midpoint + row >= column) {
+//         stair += "#";
+//       } else {
+//         stair += " ";
+//       }
+//     }
+//     console.log(stair);
+//   }
+// }
 
+function pyramid(n, row = 0, level = "") {
+  if (row === n) {
+    //if row completed return
+    return;
+  }
+  if (level.length === 2 * n - 1) {
+    //basically length determines here column single row all columns filled then console and call function with increased row no
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+
+  let midpoint = Math.floor((2 * n - 1) / 2);
+  let add;
+  if (midpoint - row <= level.length && midpoint + row >= level.length) {
+    //we are checking where we are of midpoint
+    add = "#"; //if true add will store pound
+  } else {
+    add = " "; //otherwise it will store the space
+  }
+  pyramid(n, row, (level = level + add)); //we will not change n and row but according to the condition we will update level again it will check level.length and if doesn't satisfy then again it will come for the adding pound or space then again recall
+}
+pyramid(3);
 module.exports = pyramid;
--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -16,7 +16,42 @@
 //       '##  '
 //       '### '
 //       '####'
+                          //solution 1     //watch it again and think in the form of table row and column as well as console positioning is very essential also only console.log is good
+                          //best for interview it is trick discuss in interview before solving in interview dont write straight away
+function steps(n) {
+   for(let row = 0; row< n; row++){
+    let stair = ""
+    for (let column= 0; column< n; column++){
+        if(column <= row){
+            stair+="#"
+        } else {
+            stair+=" "
+        }
+        
+    }
+    console.log(stair)
+   }
+}   
 
-function steps(n) {}
+                        //solution 2       problem by recursion must study*****
+// function steps(n, row=0, stair = " "){
+//     if (n === row){
+//         return;
+//     }
 
+//     if (n === stair.length){
+//         console.log(stair)
+//         return steps(n, row + 1)
+//     }
+
+//     if(stair.length <= row){
+//         stair += "#";
+//     } else {
+//         stair += " "
+//     }
+
+//     steps(n, row, stair)
+
+// }
+steps(2)
 module.exports = steps;
